a1b8aeba01cc773d11762f11edd055f1e86fd0a9
Anchit Agarwal <anchit008@gmail.com>
2017-04-15 17:34:19 -0700

    Updated for CelebA dataset

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 from datetime import datetime
 import tensorflow as tf
@@ -6,75 +7,33 @@ import numpy as np
 import glob
 from PIL import Image
 from dcgan import DCGAN
+import load_data
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_integer('num_examples_per_epoch_for_train', 5000,
-										"""number of examples for train""")
-tf.app.flags.DEFINE_integer('max_steps', 10001,
-										"""Number of batches to run.""")
-tf.app.flags.DEFINE_string('data_dir', '/home/anchit/lsun/',
-										"""Path to LSUN dataset.""")
-tf.app.flags.DEFINE_string('images_dir', 'images',
-										"""Directory where to write generated images.""")
-tf.app.flags.DEFINE_string('logdir', 'logdir',
-										"""Directory where to write event logs and checkpoint.""")
-tf.app.flags.DEFINE_string('file_pattern', '/home/anchit/lsun/*.webp',
-										""""File pattern for the images.""")
-
-CROP_IMAGE_SIZE = 96
-
-def read_lsun(filename_queue):
-	"""
-	Reads and parses examples from LSUN dataset.
-
-	Args:
-		filename_queue: A queue of strings with filenames to read from
-
-	Returns:
-		An object representing a single example
-	"""
-	# read an entire image file
-	image_reader = tf.WholeFileReader()
-	# read a file from the queue
-	_, image_file = image_reader.read(filename_queue)
-	# decode the image as a JPEG
-	image = tf.image.decode_jpeg(image_file)
-	image = tf.image.resize_image_with_crop_or_pad(image, CROP_IMAGE_SIZE, CROP_IMAGE_SIZE)
-	return tf.reshape(image, [CROP_IMAGE_SIZE, CROP_IMAGE_SIZE, 3])
-
-def inputs(batch_size, s_size):
-	"""
-	Construct input for LSUN dataset using Reader op.
-
-	Args:
-		batch_size: Integer, required batch size
-		s_size: Integer, desired crop image factor
-
-	Returns:
-		image: 4-D Tensor of shape ``[batch_size, height, width, channels]``
-	"""
-	# make a queue of file names including all the images files in the directory
-	filename_queue = tf.train.string_input_producer(
-								tf.train.match_filenames_once(FLAGS.file_pattern))
-	image = read_lsun(filename_queue)
-	min_queue_examples = FLAGS.num_examples_per_epoch_for_train
-	capacity = min_queue_examples + 3 * batch_size
-	image_batch = tf.train.shuffle_batch(
-										 [image],
-										 batch_size=batch_size,
-										 capacity=capacity,
-										 min_after_dequeue=min_queue_examples)
-	tf.summary.image('images', image_batch)
-	return tf.subtract(tf.div(
-		tf.cast(tf.image.resize_images(
-			image_batch, [s_size * 2 ** 4, s_size * 2 ** 4]), 'float'), 127.5), 1.0)
+tf.app.flags.DEFINE_integer('max_steps', 50001, """Number of batches to run.""")
+tf.app.flags.DEFINE_string('data_dir', '/home/anchit/lsun/', """Path to LSUN dataset.""")
+tf.app.flags.DEFINE_string('images_dir', 'images', """Directory where to write generated images.""")
+tf.app.flags.DEFINE_string('logdir', 'logdir', 
+	"""Directory where to write event logs and checkpoint.""")
 	
 def main(argv=None):
 	"""
 	Main function that calls a training batch sample and trains the model.
 	"""
-	dcgan = DCGAN(s_size=6)
-	traindata = inputs(dcgan.batch_size, dcgan.s_size)
+	if len(argv) < 2:
+		print "Please input desired dataset in cmd line: `lsun` or `celeb`."
+		sys.exit()
+
+	dcgan = DCGAN(batch_size=64, s_size=4)
+
+	traindata = None
+	if argv[1] == 'lsun':
+		# load input pipeline for LSUN dataset
+		traindata = load_data.lsun_inputs(dcgan.batch_size, dcgan.s_size)
+	elif argv[1] == 'celeb':
+		# load input pipeline for CelebA dataset
+		traindata = load_data.celeb_inputs(dcgan.batch_size, dcgan.s_size)
+
 	losses = dcgan.loss(traindata)
 
 	# feature mapping
